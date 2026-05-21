84843a710f491cc760e081a874d4920764ef4084
gfrancagm <gfrancagm@gmail.com>
2026-05-21 17:22:21 -0300

    dunno

--- src/util/CalcFreq.hs
+++ src/util/CalcFreq.hs
@@ -1,25 +1,21 @@
--- O programa deve computar, em c1 e c2, a frequencia fi (1 <= i <= 2) de cada palavra 
--- que não esteja incluída em sep.
--- Palavras contidas em res devem ter o dobro de peso que as demais palavras.
+module util.CalcFreq where
+
 import System.IO
 import Data.List (sortBy, nub)
 
-replaceSep :: String -> Char -> String
-replaceSep sep x
+retiraSeparadores :: String -> Char -> String
+retiraSeparadores sep x
     | x `elem` sep = " "
     | otherwise = [x] 
 
-programMinusSep :: String -> String -> String
-programMinusSep program sep = concatMap (replaceSep sep) program
+contaPalavra lista palavra = length (filter (== palavra) lista)
 
-countWord list word = length (filter (== word) list)
-
-sortByQtt (a1, b1) (a2, b2) 
+sorteiaPorQuantidade (a1, b1) (a2, b2) 
     | b1 > b2 = LT
     | b1 < b2 = GT
     | b1 == b2 = compare a1 a2
 
-addWeight res (a, b) 
+adicionaPesos res (a, b) 
     | a `elem` res = (a, 2*b)
     | otherwise = (a, b)
 
